53b7ee67d9ae0b1ec53f6e1889b80f92c378714b
KIM PYEONGGNAG <vudrkd34@naver.com>
2021-02-03 16:45:06 +0900

    update9

--- server/server.js
+++ server/server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const router = require('./route'); 
 
-const cors = require('cors');
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
+
+app.use('/api/data', function(req, res) {
+    res.json({ greeting: 'Hello World' });
+});
 
 app.get('/', (req, res, next) => {
     res.send('Hello, Heroku!');
@@ -15,16 +18,13 @@ const bodyParser = require('body-parser');
 
 sequelize.sync();
 
-app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.listen(port);
+app.listen(PORT);
 
 
-app.use('/api/data', function(req, res) {
-    res.json({ greeting: 'Hello World' });
-});
+
 
 app.use('/' , router);
 
