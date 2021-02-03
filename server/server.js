a21202a0d2fc67d51aea1440f2242b990d95346f
KIM PYEONGGNAG <vudrkd34@naver.com>
2021-02-03 14:30:51 +0900

    server_update2

--- server/server.js
+++ server/server.js
@@ -1,25 +1,21 @@
 const express = require('express');
 const app = express();
 const router = require('./route'); 
-const proxy = require("http-proxy-middelware");
 
-const PORT = process.env.PORT || 5000;
+const cors = require('cors');
+
+const PORT = process.env.PORT || 4000;
 const sequelize = require('./models').sequelize;
 const bodyParser = require('body-parser');
 
 sequelize.sync();
 
-
-app.use(proxy("/api/greeting", { target: "http://localhost:5000" }));
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use('/' , router);
-
-app.get("/api/greeting", (req, res) => {
-    res.send("Hello World!");
-});
 
 
 app.listen(PORT, () => {
