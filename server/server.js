da34dcc22f6a996e9d8b2caca0d69215f6a3f50d
KIM PYEONGGNAG <vudrkd34@naver.com>
2021-02-03 13:22:34 +0900

    add_cors

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const router = require('./route'); 
+const cors = require('cors');
 
 const PORT = process.env.PORT || 4000;
 const sequelize = require('./models').sequelize;
@@ -8,6 +9,7 @@ const bodyParser = require('body-parser');
 
 sequelize.sync();
 
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
