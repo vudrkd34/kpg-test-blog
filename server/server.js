f0fb44cd49309a4d113ad48cc57e1be070754974
KIM PYEONGGNAG <vudrkd34@naver.com>
2021-02-03 14:21:09 +0900

    middleware

--- server/server.js
+++ server/server.js
@@ -1,77 +1,25 @@
 const express = require('express');
 const app = express();
 const router = require('./route'); 
-const cors = require('cors');
+const proxy = require("http-proxy-middelware");
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 const sequelize = require('./models').sequelize;
 const bodyParser = require('body-parser');
 
 sequelize.sync();
 
-app.use(cors());
+
+app.use(proxy("/api/greeting", { target: "http://localhost:5000" }));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use('/' , router);
 
-
-const {
-    Teacher,
-    TEST,
-    Sequelize:{Op}
-
-} = require('./models');
-sequelize.query('SET NAMES utf8;');
-
-//등록 
-app.post('/add/data', (req, res) => {
-    console.log(" REQUEST_BODY +++++++  : " +req.body.data)
-
-    TEST.create({
-        test_col : req.body.data
-    })
-    .then( result => {
-        res.send(result)
-    })
-    .catch(err => {
-        console.log(err)
-        throw err;
-    })
-})
-
-
-
-//데이터가져오기
-app.get('/get/data',(req,res) => {
-    TEST.findAll({
-    where : { /*[Op.or] : [{ num : 1}, { test_col : '테스트다2' }] */ }
-    })
-    .then( result => {res.send(result)})
-    .catch(err => {throw err})
-})
-
-
-
-//수정
-app.post('/modify/data', (req,res) => {
-
-    TEST.update({ test_col : req.body.modify.test_col }, {
-        where : { num : req.body.modify.num}
-    })
-    .then( result => {res.send(result) })
-    .catch( err => {throw err })
-})
-
-//삭제
-app.post('/delete/data', (req,res) => {
-    TEST.destroy({
-        where : { num : req.body.delete.num}
-    })
-    .then(res.sendStatus(200))
-    .catch(err => {throw err})
-})
+app.get("/api/greeting", (req, res) => {
+    res.send("Hello World!");
+});
 
 
 app.listen(PORT, () => {
