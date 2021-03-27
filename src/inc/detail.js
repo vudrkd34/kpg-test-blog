dce5c7f8008716b087addd734d5d66e6a925ce91
kpg <vudrkd34@naver.com>
2021-03-27 19:40:39 +0900

    210327

--- src/inc/detail.js
+++ src/inc/detail.js
@@ -39,7 +39,7 @@ class Detail extends Component{
       }
 
       _getInfo2 = async() => {
-        const res = await axios.post('/get/info',{ 
+        const res = await axios.post('/get/info',{ //TEST
             'num': this.props.match.params.num,
         });
     
